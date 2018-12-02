294669bb4e92c47d3a85f5afde431e4522d9c56f
Daniel Coutinho <daniel.mar.coutinho@gmail.com>
2018-12-01 23:08:13 -0200

    yet another test

--- docs/make.jl
+++ docs/make.jl
@@ -13,5 +13,5 @@ makedocs(sitename = "RationalExpectations",
 
 deploydocs(
     deps = ["Plots"],
-    repo = "github.com/danmrc/RationalExpectations.jl.git",
+    repo = "github.com/danmrc/RationalExpectations.jl.git"
 )
